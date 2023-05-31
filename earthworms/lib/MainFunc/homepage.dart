dee882467d3f2983db26235973928a7ce51281fd
mmarchu <94669226+mmarchu@users.noreply.github.com>
2023-05-31 15:08:51 +0700

    homePage 20%

--- earthworms/lib/MainFunc/homepage.dart
+++ earthworms/lib/MainFunc/homepage.dart
@@ -5,6 +5,53 @@ class homepage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(214, 232, 219, 1),
+      body: SafeArea(
+        child: Column(
+          children: [
+            //appBar
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 25,
+                vertical: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  // icon Menu
+                  IconButton(
+                    onPressed: (){
+                      Scaffold.of(context).openDrawer();
+                    }, 
+                    icon: Icon(
+                      Icons.menu,
+                      size: 38,
+                      color: Colors.grey[800],)
+                  ),
+                  
+
+                  // icon Account
+                  IconButton(
+                    onPressed: (){
+
+                    },
+                    icon: Icon(
+                      Icons.person,
+                      size: 38,
+                      color: Colors.grey[800])
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(height: 20),
+
+            // text
+            Text('Welcome BBack')
+            
+          ],
+        )
+      ),
+    );
   }
-}
+} 
+        
